b428a4548dd70b06265c2cd450561f75291da7bc
Chase Adams <cadams@walmartlabs.com>
2015-09-01 14:51:22 -0700

    Adds deps and updates webpack

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,6 +1,7 @@
 var webpack = require("webpack");
 var path = require("path");
 var boltStandardNodeModules = path.join(__dirname, "../../", "node_modules");
+var boltNodeModules = path.join(__dirname, "../../../electrode-bolt", "node_modules");
 var CleanPlugin = require("clean-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
@@ -8,15 +9,15 @@ var autoprefixer = require("autoprefixer-stylus");
 
 module.exports = {
   cache: true,
-  context: path.join(__dirname, "client"),
+  context: path.join(process.cwd(), "client"),
   entry: "./app.jsx",
   output: {
-    path: path.join(__dirname, "dist/js"),
+    path: path.join(process.cwd(), "dist/js"),
     filename: "bundle.[hash].js"
   },
   module: {
     loaders: [
-      { test: /\.jsx?$/, include: path.join(__dirname, "client"),
+      { test: /\.jsx?$/, include: path.join(process.cwd(), "client"),
         loaders: ["babel-loader?optional=runtime"] },
       { test: /\.styl$/,
         loader: ExtractTextPlugin.extract(
@@ -31,8 +32,12 @@ module.exports = {
     use: [autoprefixer({ browsers: ["last 2 versions"] })]
   },
   resolve: {
+    root: [boltStandardNodeModules, boltNodeModules, process.cwd()],
     modulesDirectories: ["node_modules", "client", "node_modules/@walmart"],
     extensions: ["", ".js", ".jsx"]
+  },
+  resolveLoader: {
+    root: [boltNodeModules, process.cwd()]
   },
   plugins: [
     // Clean
@@ -43,19 +48,3 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin(),
 
     // Meta, debug info.
-    new webpack.DefinePlugin({
-      "process.env": {
-        // Signal production mode for React JS libs.
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-    new webpack.SourceMapDevToolPlugin(
-      "../map/bundle.[hash].js.map",
-      "\n//# sourceMappingURL=http://127.0.0.1:3001/dist/map/[url]"
-    ),
-    new StatsWriterPlugin({
-      path: path.join(__dirname, "dist/server"),
-      filename: "stats.json"
-    })
-  ]
-};
